bae5f43d0355e590182f3c67bd8b64f29301fa67
tomaz.miklavcic <tomazmiklavcic101@gmail.com>
2025-07-16 12:20:11 +0200

    Added files led_management.c/.h and made some TODOs comments

---
+++ dog_collar/LED_management.c
@@ -0,0 +1,15 @@
+#include "LED_management.h"
+
+/* 
+This module is responsible for managing the LEDS
+So the blinking and other visual indications will be handled here.
+
+TODO: 
+- Implement LED blinking for difrent states:
+- Implement LED pattern for charging
+*/
+
+
+static const char *TAG = "LED_MANAGEMENT";
+
+
---
+++ dog_collar/LED_management.h
@@ -0,0 +1,11 @@
+#ifndef LED_MANAGEMENT_H
+#define LED_MANAGEMENT_H
+
+
+
+
+
+
+
+
+#endif // LED_MANAGEMENT_H
