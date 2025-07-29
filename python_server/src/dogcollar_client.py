88dc63b8d1fdc7e9475c8f5fa0832fb439240f36
tomaz.miklavcic <tomazmiklavcic101@gmail.com>
2025-07-29 13:23:28 +0200

    Added function download_file() and sepereted the parsing for function get_file_list()

--- python_server/src/dogcollar_client.py
+++ python_server/src/dogcollar_client.py
@@ -9,13 +9,29 @@ class DogCollarClient:
 
     def get_file_list(self):
         response = requests.get(f"{self.esp_32_server_url}/files")
-        soup = BeautifulSoup(response.content, "html.parser")
+        if response.status_code == 200:
+            return self.parse_file_list(response.content)
+        else:
+            raise print(f"Failed to retrieve file list: {response.status_code}")
+
+    def parse_file_list(self, response_content):
+        soup = BeautifulSoup(response_content, "html.parser")
         file_names = []
         for link in soup.find_all("a"):
             href = link.get("href")
             if href and "download?file=" in href:
                 file_names.append(link.text)
         return file_names
+
+    def download_file(self, file_name):
+        response = requests.get(f"{self.esp_32_server_url}/download?file={file_name}")
+        if response.status_code == 200:
+            with open(file_name, 'wb') as file:
+                file.write(response.content)
+            return True
+        else:
+            print(f"Failed to download file {file_name}: {response.status_code}")
+        return False
 
 if __name__ == "__main__":
     client = DogCollarClient("http://dogcollar.local")
