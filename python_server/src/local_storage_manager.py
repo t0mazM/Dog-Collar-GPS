9f052407623d01772c009b91adc108aaf84b5925
tomaz.miklavcic <tomazmiklavcic101@gmail.com>
2025-07-29 13:53:34 +0200

    Added exceptions for readign and deletign hte file

--- python_server/src/local_storage_manager.py
+++ python_server/src/local_storage_manager.py
@@ -9,17 +9,28 @@ class LocalStorageManager:
             file.write(file_content)
 
     def get_file_locally(self, file_name):
-        with open(file_name, 'rb') as file:
-            return file.read()
-
+        try:
+            with open(file_name, 'rb') as file:
+                return file.read()
+        except FileNotFoundError:
+            print(f"File {file_name} does not exist. Cannot read.")
+            return None
+        except Exception as e:
+            print(f"Unexpected error while reading {file_name} , error: {e}")
+        return None
     def delete_file_locally(self, file_name):
-        if os.path.exists(file_name):
+        try:
             os.remove(file_name)
+            print(f"File {file_name} deleted successfully.")
+        except FileNotFoundError:
+            print(f"File {file_name} does not exist. Cannot delete.")
+        except Exception as e:
+            print(f"Unexpected error while deleting {file_name}, error: {e}")
+        return None
 
 if __name__ == "__main__":
 
     storage_manager = LocalStorageManager()
 
     content = storage_manager.get_file_locally("dog_run_173.csv")
-    print(content.decode())
     storage_manager.delete_file_locally("dog_run_173.csv")
