cd699d82cc621969fdd03805d0c65a5a5ac589d8
tomaz.miklavcic <tomazmiklavcic101@gmail.com>
2025-07-30 08:24:26 +0200

    Added gpx_converter file and made it so two seperate folders are created: raw_esp32_files gpx_files

--- python_server/src/local_storage_manager.py
+++ python_server/src/local_storage_manager.py
@@ -1,17 +1,20 @@
 import os
 
-GPS_FILES_DIR = "gps_files"
+RAW_ESP32_FILES_DIR = "raw_esp32_files"
+GPX_FILES_DIR = "gpx_files"
+
 class LocalStorageManager:
     def __init__(self):
-        os.makedirs(GPS_FILES_DIR, exist_ok=True)
+        os.makedirs(RAW_ESP32_FILES_DIR, exist_ok=True)
+        os.makedirs(GPX_FILES_DIR, exist_ok=True)
 
     def save_file_locally(self, file_name, file_content):
-        with open(os.path.join(GPS_FILES_DIR, file_name), 'wb') as file:
+        with open(os.path.join(RAW_ESP32_FILES_DIR, file_name), 'wb') as file:
             file.write(file_content)
 
     def get_file_locally(self, file_name):
         try:
-            with open(os.path.join(GPS_FILES_DIR, file_name), 'rb') as file:
+            with open(os.path.join(RAW_ESP32_FILES_DIR, file_name), 'rb') as file:
                 return file.read()
         except FileNotFoundError:
             print(f"File {file_name} does not exist. Cannot read.")
@@ -19,9 +22,10 @@ class LocalStorageManager:
         except Exception as e:
             print(f"Unexpected error while reading {file_name} , error: {e}")
         return None
+    
     def delete_file_locally(self, file_name):
         try:
-            os.remove(os.path.join(GPS_FILES_DIR, file_name))
+            os.remove(os.path.join(RAW_ESP32_FILES_DIR, file_name))
             print(f"File {file_name} deleted successfully.")
         except FileNotFoundError:
             print(f"File {file_name} does not exist. Cannot delete.")
@@ -30,7 +34,7 @@ class LocalStorageManager:
         return None
     
     def file_exists(self, file_name):
-        return os.path.exists(os.path.join(GPS_FILES_DIR, file_name))
+        return os.path.exists(os.path.join(RAW_ESP32_FILES_DIR, file_name))
 
 if __name__ == "__main__":
 
