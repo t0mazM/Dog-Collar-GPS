c50c42c5086568d7710436f58919d1b01b5fde98
tomaz.miklavcic <tomazmiklavcic101@gmail.com>
2025-07-30 07:48:22 +0200

    Refactored the function download_file to better handle errors

--- python_server/src/dogcollar_client.py
+++ python_server/src/dogcollar_client.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import local_storage_manager
 
 HTTP_OK = 200   
+DOWNLOAD_TIMEOUT = 10 
 class DogCollarClient:
     def __init__(self, esp_32_server_url):
         self.storage_manager = local_storage_manager.LocalStorageManager()
@@ -26,27 +27,44 @@ class DogCollarClient:
 
     def download_file(self, file_name):
         if self.storage_manager.file_exists(file_name):
-            print(f"File {file_name} already exists locally. Skipping download.")
+            print(f"File '{file_name}' already exists locally. Skipping download.")
             return True
-        try:
-            response = requests.get(f"{self.esp_32_server_url}/download?file={file_name}", timeout=10)
-            if response.status_code == HTTP_OK:
-                self.storage_manager.save_file_locally(file_name, response.content)
-                print(f"Downloaded {file_name}")
-                return True
-            else:
-                print(f"Failed to download file {file_name}: {response.status_code}")
-                return False
-        except requests.exceptions.RequestException as e:
-            print(f"Connection error while downloading {file_name}: {e}")
-            return False
-        
 
+        url = f"{self.esp_32_server_url}/download?file={file_name}"
+
+        try:
+            response = requests.get(url, timeout=DOWNLOAD_TIMEOUT)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(f"HTTP error while downloading '{file_name}': {e.response.status_code}")
+            return False
+        except requests.exceptions.RequestException as e:
+            print(f"Network error while downloading '{file_name}': {e}")
+            return False
+        except Exception as e:
+            print(f"Unexpected error while downloading '{file_name}': {e}")
+            return False
+
+        
+        self.storage_manager.save_file_locally(file_name, response.content)
+        print(f"Downloaded '{file_name}' successfully.")
+        return True
+    
 if __name__ == "__main__":
     client = DogCollarClient("http://dogcollar.local")
+
+    # 1) Get the list of files 
     file_names = client.get_file_list()
-    print(file_names)
 
     for file_name in file_names:
+
+        # 2) Download each file 
         client.download_file(file_name)
+
+        # 3) Open file
+        file = client.storage_manager.get_file_locally(file_name)
+
+        # 4) Convert to gpx
+        # process file - convert to gpx
+        # upload to strava
         pass
